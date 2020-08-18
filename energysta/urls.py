d4d6331da5666dd5b125e43ebdb47b4d8fcbee28
Jobayer <jobayer.hossain@cosmosgroup.com.bd>
2020-08-18 22:18:24 +0600

    local changes v9.0

--- energysta/urls.py
+++ energysta/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin2/', admin.site.urls),
     path('home/', include('backend.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
