42f8649294b53c9d347e20c46d769638ccbf4d5f
Jobayer <jobayer.hossain@cosmosgroup.com.bd>
2020-08-20 11:05:44 +0600

    local changes v18.0

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from . import views
 #from backend.dash_apps import simple_graph
-from backend.dash_apps import consumption
-from backend.dash_apps import ar_working
+# from backend.dash_apps import consumption
+# from backend.dash_apps import ar_working
 urlpatterns = [
     url('', views.index, name='index')
 ]
